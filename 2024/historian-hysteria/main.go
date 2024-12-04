6a4178100d4ee83d0a519a688395ab3e46605edb
Giovanni De Giorgio <giovannidegiorgio1999@gmail.com>
2024-12-05 00:33:15 +0100

    refactor(2024): rename folders

--- 2024/historian-hysteria/main.go
+++ 2024/historian-hysteria/main.go
@@ -35,7 +35,7 @@ func main() {
 
 Given two unordered integer array, find the total distance and similarity among them
 
-Pair up the smallest number in the left list with the smallest number in the right list, then the second-smallest left number with the second-smallest right number, and so on.
+Pair up the smallest number in the left list with the smallest number in the right list, then the red-nosed-reports-smallest left number with the red-nosed-reports-smallest right number, and so on.
 Within each pair, figure out how far apart the two numbers are; you'll need to add up all of those distances.
 
 For example, if you pair up a 3 from the left list with a 7 from the right list, the distance apart is 4; if you pair up a 9 with a 3, the distance apart is 6.
